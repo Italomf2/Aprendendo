90ea97097dbf9c5705982f034d84e66a361a9cf3
Italomf2 <italomf44@gmail.com>
2023-09-21 17:26:49 -0300

    Resolução do exercicio 37 da lista

---
+++ src/Exercicios_logicProg/Exerc37.java
@@ -0,0 +1,30 @@
+/*Calcular e apresentar o valor do volume de uma lata de óleo, utilizando a fórmula:
+     v𝑜𝑙𝑢𝑚𝑒 = 3.14159 ∗ 𝑅²∗ 𝑎𝑙𝑡𝑢𝑟𝑎.*/
+
+
+package Exercicios_logicProg;
+import java.util.Scanner;
+
+public class Exerc37 {
+
+    public static void main(String[] args) {
+
+        Scanner ler = new Scanner(System.in);
+
+        double raio, altura, volume;
+
+
+        System.out.println("Digite o valor do raio");
+        raio = ler.nextDouble();
+
+        System.out.println("Digite o valor da altura");
+        altura = ler.nextDouble();
+
+
+        volume = 3.1415 * (raio * raio) * altura;
+
+        System.out.printf("O valor do volume é %.2f", volume);
+
+    }
+
+}
